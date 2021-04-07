a60d3daafce768c0b5ac25090e848eb4d4ff8d3b
TheKey <thekey@TheKeyui-MacBookPro.local>
2021-04-07 16:36:04 +0900

    home

--- src/views/Problemset.vue
+++ src/views/Problemset.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    problemset
+    <div class="problem-container">
+    <ul class="classifier">
+      <li>문제</li>
+    </ul>
+
+    </div>
   </div>
 </template>
 
@@ -24,5 +29,29 @@ export default {
 </script>
 
 <style>
+.classifier{
+  margin-top:20px;
+  width: 83.3333%;
+}
 
+.problem-container{
+  margin-left: auto;
+  margin-right: auto;
+  padding : 40px 15px 40px 15px;
+}
+@media(min-width: 768px){
+  .problem-container{
+    width: 750px;
+  }
+}
+@media(min-width: 992px){
+  .problem-container{
+    width: 970px;
+  }
+}
+@media(min-width: 1200px){
+  .problem-container{
+    width: 1170px;
+  }
+}
 </style>
